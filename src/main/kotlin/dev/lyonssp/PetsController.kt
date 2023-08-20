c5ab5aaa59041f045b170878d7ed8ba9754a3a14
sean <sean.lyons@favordelivery.com>
2023-08-20 12:11:07 -0500

    init openapi server generation but stuck on "more than 1 route matched" errors

--- src/main/kotlin/dev/lyonssp/PetsController.kt
+++ src/main/kotlin/dev/lyonssp/PetsController.kt
@@ -1,15 +1,30 @@
 package dev.lyonssp
 
+import dev.lyonssp.petstore.api.PetsApi
+import dev.lyonssp.petstore.model.Pet
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Produces
 
 @Controller
-class PetsController {
+class PetsController : PetsApi {
     @Get
     @Produces(MediaType.TEXT_PLAIN)
     fun index(): String {
         return "Hello World"
+    }
+
+    override fun createPets(): HttpResponse<Void> {
+        return HttpResponse.ok()
+    }
+
+    override fun listPets(limit: Int?): HttpResponse<MutableList<Pet>> {
+        return HttpResponse.ok()
+    }
+
+    override fun showPetById(petId: String?): Pet {
+        return Pet(1, "Fido")
     }
 }
